5db3f7472150f6ece112e3f4852eb17cdbb99af6
Edu-Reyna <eduardor1901g@gmail.com>
2025-04-08 12:52:00 -0500

    Primer Commit

---
+++ src/app/pages/detalles-autos/detalles-autos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalles-autos',
+  imports: [],
+  templateUrl: './detalles-autos.component.html',
+  styleUrl: './detalles-autos.component.css'
+})
+export class DetallesAutosComponent {
+
+}
