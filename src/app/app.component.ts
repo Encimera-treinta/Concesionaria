8486aad5656337cf38b1fac18b72ac0c224e8d99
Encimera-treinta <146698252+Encimera-treinta@users.noreply.github.com>
2025-04-09 02:35:26 -0500

    Actualizacion, navbar
    navbar  github desk es la marca
    
    Co-Authored-By: Edu-Reyna <185151875+Edu-Reyna@users.noreply.github.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterOutlet, NavBarComponent],  // <-- NavBar aquí
+  template: `
+    <app-nav-bar></app-nav-bar>
+    <router-outlet></router-outlet>
+  `,
 })
-export class AppComponent {
-  title = 'ConcesionariaFancy';
-}
+export class AppComponent {}
